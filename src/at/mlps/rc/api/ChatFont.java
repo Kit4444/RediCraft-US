9069016bbf7b06a705041438f4ddd4236b029c05
Newt <maurice.hoefner@gmail.com>
2022-03-06 00:37:20 +0100

    CustomColor Translater Class (C: Grubsic)

--- src/at/mlps/rc/api/ChatFont.java
+++ src/at/mlps/rc/api/ChatFont.java
@@ -3,17 +3,15 @@ package at.mlps.rc.api;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import net.md_5.bungee.api.ChatColor;
+
 public class ChatFont {
 	
-	private final Pattern pattern = Pattern.compile("#[a-fA-F0-9]{6}");
-	
-	public String format(String message) {
-		Matcher match = pattern.matcher(message);
-		while(match.find()) {
-			String color = message.substring(match.start(), match.end());
-			message = message.replace(color, net.md_5.bungee.api.ChatColor.of(color) + "");
-			match = pattern.matcher(message);
-		}
-		return net.md_5.bungee.api.ChatColor.translateAlternateColorCodes('&', message);
+	private static final Pattern HEX_PATTERN = Pattern.compile("#[0-9a-fA-F]{6}");
+
+	public static String translate(String text){
+		Matcher matcher = HEX_PATTERN.matcher(text);
+		while(matcher.find()){ text = text.replace(matcher.group(), ChatColor.of(matcher.group()).toString()); }
+		return text;
 	}
 }
